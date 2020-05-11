16f937398605bfeba3df91032483c8768a686e27
Krzysztof Gryko <krzysztofgryko@poczta.fm>
2020-05-11 21:59:14 +0200

    zmiany na galezi krzysztof

---
+++ AplikacjaStomatologa/AplikacjaStomatologiczna/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplikacjaStomatologiczna
+{
+    public class GlupiaKlasa
+    {
+        private int ii;
+        public int Calkowita
+        {
+            
+            get { return ii; }
+            set { ASCIIEncoding = value; }
+        }
+    }
+}
